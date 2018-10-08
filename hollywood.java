6aa0b1bb4c520c686aee5bc0fd6445a6a8871c52
Aanchu123 <43898977+Aanchu123@users.noreply.github.com>
2018-10-08 22:29:39 +0530

    Add files via upload

---
+++ hollywood.java
@@ -0,0 +1,70 @@
+//IF_I_STAY
+import java.util.Scanner;
+public class hollywood
+{
+    public static void main()
+    {
+         Scanner sc=new Scanner(System.in);
+        int f=0;int c=9;int x=0;
+        char h[]={'Ⓗ','Ⓞ','Ⓛ','Ⓛ','Ⓨ','Ⓦ','Ⓞ','Ⓞ','Ⓓ'};
+        char m[]={'I','_',' ','I',' ','_','_','A','_'};
+        while(f==0)
+        {
+            System.out.println("Your challenge :\n");
+            for(int i=0;i<9;i++)
+            System.out.print(m[i]);
+            System.out.println("\n");
+            for(int i=0;i<9;i++)
+            System.out.print(h[i]);
+            System.out.println("\nEnter your guess:\n");
+            char ch=sc.next().charAt(0);
+            if(ch=='f')
+            {
+                System.out.println("CONGRATULATIONS YOUR GUESS IS CORRECT");
+                m[1]='F';
+                for(int i=0;i<9;i++)
+                System.out.print(m[i]);
+            }
+            if(ch=='s')
+            {
+                System.out.println("CONGRATULATIONS YOUR GUESS IS CORRECT");
+                m[5]='S';
+                for(int i=0;i<9;i++)
+                System.out.print(m[i]);
+            }
+            if(ch=='t')
+            {
+                System.out.println("CONGRATULATIONS YOUR GUESS IS CORRECT");
+                m[6]='T';
+                for(int i=0;i<9;i++)
+                System.out.print(m[i]);
+            }
+            if(ch=='y')
+            {
+                System.out.println("CONGRATULATIONS YOUR GUESS IS CORRECT");
+                m[8]='Y';
+                for(int i=0;i<9;i++)
+                System.out.print(m[i]);
+            }
+            else
+            {
+                System.out.println("You lost a chance");
+                h[x]=' ';
+                for(int i=0;i<9;i++)
+                System.out.print(h[i]);
+                x++;
+                if(x==9)
+                {
+                    System.out.println("YOU LOST");
+                    f++;
+                }
+            }
+            if(m[1]=='F' && m[5]=='S' && m[6]=='T' && m[8]=='Y')
+            { System.out.println("YOU WON");
+                break;
+            }
+        }
+    }
+}
+                
+            
